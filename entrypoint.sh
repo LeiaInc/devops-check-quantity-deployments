a0a64be72f32ebb8d3fd61b81d2e087d6e2b0bc1
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-08-05 19:08:54 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,11 @@ aws eks update-kubeconfig --name "${EKS_NAME}"
 
 REPO_COUNT=$(kubectl get pods -A --template '{{range.items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep "${ECR_NAME}" -c)
 
-echo "$REPO_COUNT"
-
 if [ "$REPO_COUNT" -ge "$QTD" ];
 then
   echo "::error::You should have less than $QTD apps deployed to deploy a new branch or commit."
   echo "::error::Branch deploy failure. Please delete old or uneeded branches from your repository via Github."
   exit 1
 else
-  echo "::debug::Deploying ${REPO_COUNT} app right now..."
+  echo "Deploying ${REPO_COUNT} app right now..."
 fi
