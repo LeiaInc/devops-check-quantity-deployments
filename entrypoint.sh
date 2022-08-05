5123f7171100b4341817bb82548690aaff836fb8
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-08-05 19:07:20 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,8 @@ echo "$REPO_COUNT"
 
 if [ "$REPO_COUNT" -ge "$QTD" ];
 then
-  echo "::error::You should have less than {$QTD} apps deployed to deploy a new branch or commit."
+  echo "::error::You should have less than $QTD apps deployed to deploy a new branch or commit."
   echo "::error::Branch deploy failure. Please delete old or uneeded branches from your repository via Github."
-#  echo "ERROR: You should have less than {$QTD} apps deployed to deploy a new branch or commit.
-#Branch deploy failure. Please delete old or uneeded branches from your repository via Github."
   exit 1
 else
   echo "::debug::Deploying ${REPO_COUNT} app right now..."
